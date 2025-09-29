f6efee81a50a0f950b0d3519ef6eb5d8fa4d4cb7
PC1 <aminamro2012@gmail.com>
2025-09-30 02:03:37 +0300

    Creating backend

---
+++ ProjectsRequirements_API/Models/Requirement.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectsRequirements_API.Models
+{
+    public class Requirement
+    {
+        public int Id { get; set; }
+
+        // FK -> Iterations
+        [ForeignKey(nameof(Iteration))]
+        public int IterationId { get; set; }
+        public Iteration Iteration { get; set; } = null!;
+
+        // FK -> Categories
+        [ForeignKey(nameof(Category))]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } = null!;
+
+        [Required]
+        public string Text { get; set; } = null!; // Long text. If you want to bound it, add [MaxLength(n)]
+
+        // FK -> Priorities (optional)
+        [ForeignKey(nameof(Priority))]
+        public int? PriorityId { get; set; }
+        public Priority? Priority { get; set; }
+
+        // FK -> Severities (optional)
+        [ForeignKey(nameof(Severity))]
+        public int? SeverityId { get; set; }
+        public Severity? Severity { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
